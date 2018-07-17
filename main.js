b50aab56fabc98e6864788d7db62ce56cc7d3639
cuilei5205189 <allen5205189@gmail.com>
2018-07-17 19:57:37 +0800

    设定localstorage，数据可以存储在浏览器

--- main.js
+++ main.js
@@ -9,11 +9,19 @@ var keysToUrls = {
     'q': 'qq.com',
     'w': 'wordpress.com',
     'e': 'ele.me',
-
     'z': 'baidu.com'
 }
 
 
+var keysToUrlsFromLocal = JSON.parse(localStorage.getItem('localkeysToUrlsFromLocal'))
+
+
+//防止对浏览器local storge清理后，keysToUrlsFromLocal的值变成null
+if (keysToUrlsFromLocal === null) {
+    keysToUrlsFromLocal = keysToUrls
+} else {
+    keysToUrlsFromLocal = JSON.parse(localStorage.getItem('localkeysToUrlsFromLocal'))
+}
 
 for (index1 = 0; index1 < allKeys.length; index1++) {
     var keyRow = document.createElement('div')
@@ -30,7 +38,8 @@ for (index1 = 0; index1 < allKeys.length; index1++) {
         editButton.id = keyText
         key.className = 'key'
         img.className = 'logo'
-        var url = keysToUrls[keyText]
+        console.log(keysToUrlsFromLocal);
+        var url = keysToUrlsFromLocal[keyText]
         img.src = 'http://' + url + '/favicon.ico'
         img.id = keyText
         keyRow.appendChild(key)
@@ -38,16 +47,22 @@ for (index1 = 0; index1 < allKeys.length; index1++) {
         key.appendChild(img)
         key.appendChild(editButton)
         editButton.addEventListener('click', function (e) {
-            console.log(e.target.id)
             var input = "输入"
             var userInput = prompt(input, 'google.com')
             inputImg = document.getElementById(e.target.id)
+            keysToUrlsFromLocal[e.target.id] = userInput
+            var keysToUrlsFromLocalToJson = JSON.stringify(keysToUrlsFromLocal)
+            localStorage.setItem('localkeysToUrlsFromLocal', keysToUrlsFromLocalToJson)
             inputImg.src = 'http://' + userInput + '/favicon.ico'
         })
     }
 }
 
+
+
+
+
 document.addEventListener('keypress', function (e) {
-    var url = 'http://' + keysToUrls[e.key]
+    var url = 'http://' + keysToUrlsFromLocal[e.key]
     open(url)
 })
