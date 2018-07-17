bb2924f824e17cea98e4164c14d9d3a97a7591ec
cuilei5205189 <allen5205189@gmail.com>
2018-07-17 21:26:53 +0800

    优化代码结构

--- main.js
+++ main.js
@@ -13,8 +13,40 @@ var keysToUrls = {
 }
 
 
-var keysToUrlsFromLocal = JSON.parse(localStorage.getItem('localkeysToUrlsFromLocal'))
+function CreateHtmlTag(tagname, TagProperties) {
+    var tag = document.createElement(tagname)
+    for (i in TagProperties) {
+        tag[i] = TagProperties[i]
+    }
+    return tag
+}
 
+function ClickEditButton(button) {
+    button.addEventListener('click', function (e) {
+        var input = "输入网址"
+        var userInput = prompt(input, 'google.com')
+        inputImg = document.getElementById(e.target.id)
+        inputImg.src = 'http://' + userInput + '/favicon.ico'
+        keysToUrlsFromLocal[e.target.id] = userInput
+        setLocalStorage(keysToUrlsFromLocal)
+    })
+}
+
+function setLocalStorage(objectData) {
+    var jsonData = JSON.stringify(objectData)
+    var setData = localStorage.setItem('localkeysToUrlsFromLocal', jsonData)
+    return setData
+}
+
+function getLocalStorage(jsondata) {
+    var getJsonData = localStorage.getItem(jsondata)
+    var objectData = JSON.parse(getJsonData)
+    return objectData
+}
+
+
+
+var keysToUrlsFromLocal = getLocalStorage('localkeysToUrlsFromLocal')
 
 //防止对浏览器local storge清理后，keysToUrlsFromLocal的值变成null
 if (keysToUrlsFromLocal === null) {
@@ -24,43 +56,34 @@ if (keysToUrlsFromLocal === null) {
 }
 
 for (index1 = 0; index1 < allKeys.length; index1++) {
-    var keyRow = document.createElement('div')
-    keyRow.className = 'keyRow'
+    var keyRow = CreateHtmlTag('div', {
+        'className': 'keyRow'
+    })
     var table = document.getElementById('table')
     table.appendChild(keyRow)
     for (index2 = 0; index2 < allKeys[index1].length; index2++) {
         var keyText = allKeys[index1][index2]
-        var key = document.createElement('kbd')
-        var img = document.createElement('img')
-        var editButton = document.createElement('button')
-        editButton.className = 'edit'
-        editButton.textContent = 'e'
-        editButton.id = keyText
-        key.className = 'key'
-        img.className = 'logo'
-        console.log(keysToUrlsFromLocal);
         var url = keysToUrlsFromLocal[keyText]
-        img.src = 'http://' + url + '/favicon.ico'
-        img.id = keyText
+        var key = CreateHtmlTag('kbd', {
+            'className': 'key',
+            'textContent': keyText,
+        })
+        var img = CreateHtmlTag('img', {
+            'className': 'logo',
+            'id': keyText,
+            'src': 'http://' + url + '/favicon.ico'
+        })
+        var editButton = CreateHtmlTag('button', {
+            'clasName': 'edit',
+            'textContent': 'edit',
+            'id': keyText,
+        })
         keyRow.appendChild(key)
-        key.textContent = keyText
         key.appendChild(img)
         key.appendChild(editButton)
-        editButton.addEventListener('click', function (e) {
-            var input = "输入"
-            var userInput = prompt(input, 'google.com')
-            inputImg = document.getElementById(e.target.id)
-            keysToUrlsFromLocal[e.target.id] = userInput
-            var keysToUrlsFromLocalToJson = JSON.stringify(keysToUrlsFromLocal)
-            localStorage.setItem('localkeysToUrlsFromLocal', keysToUrlsFromLocalToJson)
-            inputImg.src = 'http://' + userInput + '/favicon.ico'
-        })
+        ClickEditButton(editButton)
     }
 }
-
-
-
-
 
 document.addEventListener('keypress', function (e) {
     var url = 'http://' + keysToUrlsFromLocal[e.key]
